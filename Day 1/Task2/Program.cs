0c89b395cb303a6a446fa4dfc532b5abd01a7841
PLUMBIR <bondarovich.kirya@gmail.com>
2024-04-16 11:41:57 +0300

    Added tasks from day 1

---
+++ Day 1/Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите трехзначное число: ");
+            int ABC = int.Parse(Console.ReadLine());
+
+            int B = (ABC % 100) / 10;
+            int C = ABC % 10;
+            int A = ABC / 100;
+
+            int BCA = B * 100 + C * 10 + A;
+
+            Console.WriteLine($"Полученное число: {BCA}");
+        }
+    }
+}
---
+++ Day 1/Task4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Task4
+{
+    public class Program
+    {
+        static double Distance(double x1, double y1, double x2, double y2)
+        {
+            return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+        }
+
+        static void Main()
+        {
+
+            Console.Write("Введите координаты точки A (xA, yA): ");
+            double xA = double.Parse(Console.ReadLine());
+            double yA = double.Parse(Console.ReadLine());
+
+            Console.Write("Введите координаты точки B (xB, yB): ");
+            double xB = double.Parse(Console.ReadLine());
+            double yB = double.Parse(Console.ReadLine());
+
+            Console.Write("Введите координаты точки C (xC, yC): ");
+            double xC = double.Parse(Console.ReadLine());
+            double yC = double.Parse(Console.ReadLine());
+
+            double AB = Distance(xA, yA, xB, yB);
+            double BC = Distance(xB, yB, xC, yC);
+            double AC = Distance(xA, yA, xC, yC);
+
+            double perimeter = AB + BC + AC;
+            double p = perimeter / 2;
+            double area = Math.Sqrt(p * (p - AB) * (p - BC) * (p - AC));
+
+            Console.WriteLine($"Периметр треугольника P = {perimeter}");
+            Console.WriteLine($"Площадь треугольника S = {area}");
+
+            Console.ReadLine();
+        }
+    }
+}
